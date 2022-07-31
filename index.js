9373a6262657a7bd4b183d73e45bd77ac57b12ef
Prathamesh Karanjkar <karanjkarprathamesh0728@gmail.com>
2022-07-31 12:26:37 +0530

    fixed some bugs..in js file!

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO List Manager
 
 $('document').ready(() => {
-
     // Storing in localstorage..
 
     let table = document.getElementById('table');
@@ -29,13 +28,13 @@ $('document').ready(() => {
                 convertedInfo.push([taskID, taskName]);
                 localStorage.setItem('itemJson', JSON.stringify(convertedInfo));
                 table.innerHTML += `
-         <tr class='info'>
-         <td>${taskID}</td>
-         <td>${taskName}</td>
-         <td><button class="dButton" id="${id}" onclick="deleteRow()">Delete</button>
-         <button class= "eButton" id="editRow" onclick="editRow()">Edit</button>
-         </td>
-         </tr>`
+                <tr class='info'>
+                <td>${taskID}</td>
+                <td>${taskName}</td>
+                <td><button class="dButton" id="${id}" onclick="deleteRow()">Delete</button>
+                <button class= "eButton" id="editRow" onclick="editRow()">Edit</button>
+                </td>
+                </tr>`
             };
         };
     };
@@ -44,29 +43,34 @@ $('document').ready(() => {
 
     let StroredInfoInJson = localStorage.getItem('itemJson');
     let inJson = JSON.parse(StroredInfoInJson);
-    let id = 0;
-    for (let row = 0; row < inJson.length; row++) {
-        table.innerHTML += `
-         <tr class='info'>
-         <td>${inJson[row][0]}</td>
-         <td>${inJson[row][1]}</td>
-         <td><button class= "dButton" onclick="deleteRow(${id})">Delete</button>
-         <button class= "eButton" id="editRow" onclick="editRow(${id})">Edit</button>
-         </td>
-         </tr>`
-        id += 1;
+
+    refresh = () => {
+        let id = 0;
+        for (let row = 0; row < inJson.length; row++) {
+            table.innerHTML += `
+            <tr class='info'>
+            <td>${inJson[row][0]}</td>
+            <td>${inJson[row][1]}</td>
+            <td><button class= "dButton" onclick="deleteRow(${id})">Delete</button>
+            <button class= "eButton" id="editRow" onclick="editRow(${id})">Edit</button>
+            </td>
+            </tr>`
+            id += 1;
+        };
     };
+    // refresh();
 
     // For deleting info..
 
     deleteRow = (id) => {
-        // alert(id);
+        alert(id);
         let retrieveForDelete = localStorage.getItem('itemJson');
         let retrieveForConvert = JSON.parse(retrieveForDelete);
         retrieveForConvert.splice(id, 1);
         // console.log(retrieveForConvert);
         localStorage.setItem('itemJson', JSON.stringify(retrieveForConvert));
     };
+    refresh();                   // Rearranging id again after deleting any task..
 
     // For editing info..
 
